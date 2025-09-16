46c9f55f9e978dc08e6737bbe5f744443a2f2962
MHamzaAhmad <hmzaawan88@gmail.com>
2025-09-16 11:38:22 +0500

    refactor: Remove prerun_checks from start_proxy.py to streamline startup process

--- litellm/start_proxy.py
+++ litellm/start_proxy.py
@@ -1,5 +1,5 @@
 import asyncio
-from litellm.proxy.proxy_server import app, prerun_checks  # Import to trigger any pre-loads
+from litellm.proxy.proxy_server import app  # Import to trigger any pre-loads
 from litellm.proxy.utils import ProxyConfig  # Utility for loading config
 import uvicorn
 
@@ -8,8 +8,6 @@ proxy_config = ProxyConfig()
 config_path = "/app/litellm/config.yaml"  # Set your config path here
 config = proxy_config.load_config(config_file=config_path)
 
-# Optional: Run any pre-startup checks or initializations if needed
-asyncio.run(prerun_checks())
 
 if __name__ == "__main__":
     uvicorn.run(app, uds="/app/litellm.sock", workers=1)
